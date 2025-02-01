e0ee9f7ccc7cc38dafa277f053b33e88df24ed56
YotamGold <yotam.goldberg@gmail.com>
2025-02-01 16:57:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+clean_image = median(image, ball(3))
+edge = edge_detection(clean_image)
+binary_image = edge > 90
+edge_image = Image.fromarray(binary_image)
+edge_image.save('my_edges.png')
 
+plt.imshow(binary_image, cmap='gray')
+plt.show()
